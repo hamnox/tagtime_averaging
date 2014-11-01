9e904747ded4f13966fd8c36fdbffa02a6164390
hamnox <mheisey.nox@gmail.com>
2014-11-01 11:02:03 -0600

    I DONE GRAPH'D IT

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ tagtime_averaging
 =================
 Works with TagTime and Beeminder
 
-Input a local .log file, the average of any numerical tags in a day gets sent to beeminder.
-This is set for just me, fork and fix the auth_token and post link if you want to use it.
+Pulls information from local .log files, and lists the daily average value of any numerical tags given.
--- tagtimeAverage.py
+++ tagtimeAverage.py
@@ -2,7 +2,8 @@ import time
 import copy
 import requests
 import numpy as np
-
+import glob
+		
 def elementwise_number(num):
 	return_value = np.empty_like(num)
 	for cnt,x in enumerate(num):
@@ -15,17 +16,23 @@ def elementwise_number(num):
 			return_value[cnt] = True
 	return return_value
 	
-while True:
-	filename = raw_input("Log File -->")
-#	print filename
-	try:
-		file = open(filename,'r')
-		break
-	except:
-		pass
-#time.sleep(5) # this makes it sleep for 5 seconds
-log = file.read()
-file.close()
+
+# while True:
+	# filename = raw_input("Log File -->")
+# #	print filename
+	# try:
+		# file = open(filename,'r')
+		# break
+	# except:
+		# pass
+
+globby = list(glob.glob('*.log'))
+log = ""
+
+for filename in globby:
+	file = open(filename,'r')
+	log += file.read()
+	file.close()
 
 # location = 0
 loglist = []
@@ -36,10 +43,8 @@ for ping in temp:
 	except:
 		continue
 	# pingday = ping[ping.find('  [')+8:ping.find('  [')+13]
-	pingday = time.strftime("%d",time.localtime(pingstamp))
+	pingday = time.strftime("%j",time.localtime(pingstamp))
 	ping = ping[ping.find(" ")+1:ping.find('  [')].split()
-	if ping == []:
-		continue
 	for tag in ping:
 		if tag.isdigit():
 			loglist.append([int(pingday),int(tag),int(pingstamp)])
@@ -50,17 +55,35 @@ for ping in temp:
 
 by_day_array = np.array(loglist)
 datapoints = []
+print np.max(by_day_array[:,0])
 # print by_day_array[:,1]
-for day in np.arange(np.min(by_day_array[:,0]),np.max(by_day_array[:,0])):
+for day in np.arange(np.min(by_day_array[:,0]),np.max(by_day_array[:,0])+1):
 	daystract = np.logical_and(np.equal(by_day_array[:,0],day*np.ones_like(by_day_array[:,0])), elementwise_number(by_day_array[:,0]))
 	temp = np.compress(daystract,by_day_array,0)
 	if np.count_nonzero(temp) == 0:
 		continue
-	datapoints.append({"id":time.strftime("%m%d",time.localtime(np.min(temp[:,2]))) + "x" + str(np.count_nonzero(temp[:,2])), "timestamp":np.max(temp[:,2]),"value":np.mean(temp[:,1]),"comment":"STD: " + str(round(np.std(temp[:,1]),3))})
-for x in datapoints:
-	print x
-payload = {"auth_token":"eq4wJw3enxsFU5yptLsx", "datapoints":str(datapoints).replace("'",'"')}
-r = requests.post("https://www.beeminder.com/api/v1/users/hamnox/goals/happiness/datapoints/create_all.json",params=payload)
+	#array is [0: month-day string, 1: dayofyear, 2: value, 3: 1st IQ, 4: 3rd IQ, 5: totaltags]
+	datapoints.append([time.strftime("%m.%d",time.localtime(np.min(temp[:,2]))), time.struct_time(time.localtime(np.min(temp[:,2])))[7],np.mean(temp[:,1]), np.mean(temp[:,1])-np.percentile(temp[:,1],25),np.percentile(temp[:,1],75)-np.mean(temp[:,1]), np.count_nonzero(temp[:,2])])
+
+for data in datapoints:
+	print "%s Average: %s (IQR: %s-%s)" % (data[0], data[2], data[2]-data[3],data[2]+data[4])
+
+import matplotlib.pyplot as plt
+
+
+datapoints = np.array(datapoints)
+xlabels = datapoints[:,0]
+print datapoints
+datapoints = datapoints.astype(np.float)
+
+plt.figure()
+
+plt.plot(datapoints[:,1], datapoints[:,2], 'b-')
+plt.errorbar(datapoints[:,1], datapoints[:,2],yerr=(datapoints[:,3],datapoints[:,4]))
+plt.ylim((0,10))
+plt.xlim((np.min(datapoints[:,1])-1,np.max(datapoints[:,1])+1))
+plt.xticks(datapoints[:,1], xlabels)
+plt.show()
 
 # {x[0]:[0,0] for x in loglist}   depreciated upon deciding to do stuff by averages.
 ### depreciated section
@@ -100,7 +123,7 @@ r = requests.post("https://www.beeminder.com/api/v1/users/hamnox/goals/happiness
 # print "\n\n"
 # print(r.text)
 
-print "done"
+#print "done"
 
 #while location <= len(log):
 #	temp = log[location:log.find("]",location)+1]
